cfc5132259044e2ccd96c5f3aac9ec4b630e9549
A01252512 <a01252512@itesm.mx>
2021-09-24 16:20:14 +0000

    Version1

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,20 @@
+def cant_tarjetas(pliego, plumon):
+    tarjetasPlumon = plumon * 35
+    tarjetasPliego = pliego * 12
+
+    if tarjetasPlumon >= tarjetasPliego:
+        return tarjetasPliego
+
+    else:
+        return tarjetasPlumon
 
 def main():
     #escribe tu código abajo de esta línea
-    
+    pliegos = int(input('Dame la cantidad de pliegos de papel albanene: '))
+    plumones = int(input('Dame la cantidad de plumones: '))
+    tarjetasMax = cant_tarjetas(pliegos, plumones)
 
+    print(f'El número máximo de tarjetas que se pueden hacer es: {tarjetasMax}')
+    
 if __name__=='__main__':
     main()
